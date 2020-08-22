8c568870ea77f3c21bdfadf69417402abbb54ed8
cohen-salama <sergiocohensalama@pursuit.org>
2020-08-22 11:35:43 -0400

    assessment

--- unitOneAssessment.js
+++ unitOneAssessment.js
@@ -5,31 +5,83 @@ let assert = require('assert')
 // Write a function called isOdd that returns whether or not a number is odd.
 // If something that is not a number is passed in, return false.
 
+const isOdd = (num) => {
+  if (num % 2 === 0 || !(num % 2)) {
+    return false
+  }
+  if (num % 2 !== 0) {
+    return true
+  }
+}
 
 // Uncomment out the next line to test your solution
-// runQ1Tests()
+runQ1Tests()
 
 
 // Question Two:
 
 // Write a function called numberOfDigits that returns how many digits are in a given number
 
+numberOfDigits = (num) => {
+  string = num.toString()
+  return string.length
+}
+
 // Uncomment out the next line to test your solution
-// runQ2Tests()
+runQ2Tests()
 
 // Question Three:
 
 // Write a function called disemvowel that removes all of the vowels from a string.
 // Treat y as a consonant, not a vowel
 
+disemvowel = (string) => {
+  disemvoweled = ''
+  for (let char of string) {
+    if (char !== 'a' &&
+        char !== 'e' &&
+        char !== 'i' &&
+        char !== 'o' &&
+        char !== 'u' &&
+        char !== 'A' &&
+        char !== 'E' &&
+        char !== 'I' &&
+        char !== 'O' &&
+        char !== 'U') {
+      disemvoweled+= char  
+    }
+  }
+  return disemvoweled
+}
+
 // Uncomment out the next line to test your solution
-// runQ3Tests()
+runQ3Tests()
 
 // Question Four:
 // Write a function called secondSmallest that returns the second smallest number in an array
 
+secondSmallest = (array) => {
+  let min = Number.POSITIVE_INFINITY
+
+  for (let num of array) {
+    if (num < min) {
+      min = num
+    } 
+  }
+
+  let secondMin = Number.POSITIVE_INFINITY
+  
+  for (let num of array) {
+    if (num > min && num < secondMin) {
+      secondMin = num
+    }
+  }
+
+  return secondMin
+}
+
 // Uncomment out the next line to test your solution
-// runQ4Tests()
+runQ4Tests()
 
 // Question Five:
 // Write a function called getLocations that takes in an array of objects that look like the array below,
@@ -42,8 +94,18 @@ let assert = require('assert')
 // Sample output:
 // ["Algeria", "Belize", "China", "Denmark"]
 
+const getLocations = (array) => {
+  let locations = []
+
+  for (let object of array) {
+    locations.push(object.location)
+  }
+
+  return locations
+}
+
 // Uncomment out the next line to test your solution
-// runQ5Tests()
+runQ5Tests()
 
 
 // Question Six:
@@ -51,8 +113,14 @@ let assert = require('assert')
 // Write a function called onlyOddStrings that takes in an array of strings as input and returns an array that only includes strings with an odd number of characters
 // Your function should use a higher-ordered function (e.g map, filter, reduce, every, sort) in its implementation
 
+const onlyOddStrings = (array) => {
+  oddStrings = array.filter(string => string.length % 2 !== 0) 
+
+  return oddStrings
+}
+
 // Uncomment out the next line to test your solution
-// runQ6Tests()
+runQ6Tests()
 
 
 // Question Seven:
@@ -70,8 +138,24 @@ let assert = require('assert')
 // Make a function called getAllDayDescriptions that takes in an array of Day objects and returns an array of their descriptions.  Use a higher-ordered function (e.g map, filter, reduce, every, sort) in your implementation.
 // The output should be in the same order as the input
 
+class Day {
+  constructor(temperature, weather) {
+    this.temperature = temperature
+    this.weather = weather
+  }
+
+  getDescription() {
+    return `It is ${this.temperature} degrees and ${this.weather}`
+  }
+}
+
+const getAllDayDescriptions = (array) => {
+  let descriptions = array.map(day => day.getDescription())
+  return descriptions
+}
+
 // Uncomment out the next line to test your solution
-// runQ7Tests()
+runQ7Tests()
 
 
 
