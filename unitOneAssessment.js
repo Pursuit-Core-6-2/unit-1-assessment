16fb4cc810092494ca4cebac44e9eb2849bbe7a5
vonbarown <vonielbrown@pursuit.org>
2020-08-22 11:48:51 -0400

    some answers

--- unitOneAssessment.js
+++ unitOneAssessment.js
@@ -1,41 +1,79 @@
-let assert = require('assert')
+let assert = require("assert");
 
 // Question One:
 
 // Write a function called isOdd that returns whether or not a number is odd.
 // If something that is not a number is passed in, return false.
+const isOdd = (param) => {
+  if (!param && Number(param) !== NaN) {
+    return false;
+  }
+  console.log(Number(param) !== NaN, !param);
 
+  return Number(param) % 2 !== 0;
+};
 
 // Uncomment out the next line to test your solution
-// runQ1Tests()
-
+runQ1Tests();
 
 // Question Two:
 
 // Write a function called numberOfDigits that returns how many digits are in a given number
-
+const numberOfDigits = (num) => {
+  let str = num.toString();
+  return str.length;
+};
 // Uncomment out the next line to test your solution
-// runQ2Tests()
+// runQ2Tests();
 
 // Question Three:
 
 // Write a function called disemvowel that removes all of the vowels from a string.
 // Treat y as a consonant, not a vowel
+const disemvowel = (str) => {
+  const vowelObj = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true,
+  };
 
+  let ans = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (!vowelObj[str[i].toLowerCase()]) {
+      ans += str[i];
+    }
+  }
+  return ans;
+};
 // Uncomment out the next line to test your solution
-// runQ3Tests()
+// runQ3Tests();
 
 // Question Four:
 // Write a function called secondSmallest that returns the second smallest number in an array
+const secondSmallest = (arr) => {
+  arr.sort((a, b) => a - b);
 
+  return arr[1];
+};
 // Uncomment out the next line to test your solution
-// runQ4Tests()
+// runQ4Tests();
 
 // Question Five:
 // Write a function called getLocations that takes in an array of objects that look like the array below,
 // and returns an array of the strings corresponding to the value of the location property
 // The output should be in the same order as the input
+const getLocations = (arr) => {
+  let res = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    res[i] = arr[i].location;
+  }
+
+  return res;
+};
 // Sample input:
 // [{location: "Algeria", population: 41}, {location: "Belize", population: 0.4}, {location: "China", population: 1386}, {location: "Denmark", population: 6}]
 
@@ -43,17 +81,19 @@ let assert = require('assert')
 // ["Algeria", "Belize", "China", "Denmark"]
 
 // Uncomment out the next line to test your solution
-// runQ5Tests()
-
+// runQ5Tests();
 
 // Question Six:
 
 // Write a function called onlyOddStrings that takes in an array of strings as input and returns an array that only includes strings with an odd number of characters
 // Your function should use a higher-ordered function (e.g map, filter, reduce, every, sort) in its implementation
+const onlyOddStrings = (arr) => {
+  if (arr.length === 0) return [];
 
+  return arr.filter((el) => el.length % 2 !== 0);
+};
 // Uncomment out the next line to test your solution
-// runQ6Tests()
-
+// runQ6Tests();
 
 // Question Seven:
 
@@ -61,7 +101,16 @@ let assert = require('assert')
 // Make a class called Day
 // Give it two properties set by the constructor named temperature and weather
 // Give it a method named getDescription that returns a string in the format described below
+class Day {
+  constructor(temperature, weather) {
+    this.temperature = temperature;
+    this.weather = weather;
+  }
 
+  getDescription() {
+    return `It is ${this.temperature} degrees and ${this.weather}`;
+  }
+}
 // Example
 // let myDay = Day(80, "sunny")
 // myDay.getDescription() // returns "It is 80 degrees and sunny"
@@ -70,43 +119,48 @@ let assert = require('assert')
 // Make a function called getAllDayDescriptions that takes in an array of Day objects and returns an array of their descriptions.  Use a higher-ordered function (e.g map, filter, reduce, every, sort) in your implementation.
 // The output should be in the same order as the input
 
+const getAllDayDescriptions = (arr) => {
+  return arr.map((el) => el.getDescription());
+};
 // Uncomment out the next line to test your solution
-// runQ7Tests()
-
-
+// runQ7Tests();
 
 // The code below is used to test your solutions.  Feel free to look over it, but do not change any of it.
 
 function TestCase(input, output) {
-  this.input = input
-  this.output = output
+  this.input = input;
+  this.output = output;
   this.formattedInput = () => {
-    return JSON.stringify(this.input)
-  }
+    return JSON.stringify(this.input);
+  };
 }
 
 function runTests(questionNum, testCases, testCallback) {
-  console.log(`Question ${questionNum} Tests`)
+  console.log(`Question ${questionNum} Tests`);
   try {
     for (let testCase of testCases) {
-      console.log(`Running ${testCallback.name}(${testCase.formattedInput()})`)
-      assert.strictEqual(JSON.stringify(testCallback(testCase.input)), JSON.stringify(testCase.output))
+      console.log(`Running ${testCallback.name}(${testCase.formattedInput()})`);
+      assert.strictEqual(
+        JSON.stringify(testCallback(testCase.input)),
+        JSON.stringify(testCase.output)
+      );
     }
-    console.log(`All Question ${questionNum} tests passed!\n`)
-  }
-  catch(error) {
+    console.log(`All Question ${questionNum} tests passed!\n`);
+  } catch (error) {
     if (error.expected === undefined) {
-      console.log("An unexpected error occurred running the test")
-      console.log(error)
+      console.log("An unexpected error occurred running the test");
+      console.log(error);
     } else {
-      console.log(`\nTest failed.  Was expecting "${error.expected}", but got "${error.actual}"`)
+      console.log(
+        `\nTest failed.  Was expecting "${error.expected}", but got "${error.actual}"`
+      );
     }
   }
 }
 
 function runQ1Tests() {
   let testCases = [
-    new TestCase(1,true),
+    new TestCase(1, true),
     new TestCase(3, true),
     new TestCase(5, true),
     new TestCase(7, true),
@@ -116,19 +170,19 @@ function runQ1Tests() {
     new TestCase(4, false),
     new TestCase(10, false),
     new TestCase(NaN, false),
-    new TestCase("hi", false)
-  ]
-  runTests("One", testCases, isOdd)
+    new TestCase("hi", false),
+  ];
+  runTests("One", testCases, isOdd);
 }
 
 function runQ2Tests() {
-    let testCases = [
-      new TestCase(4,1),
-      new TestCase(14,2),
-      new TestCase(8473,4),
-      new TestCase(73746, 5)
-    ]
-    runTests("Two", testCases, numberOfDigits)
+  let testCases = [
+    new TestCase(4, 1),
+    new TestCase(14, 2),
+    new TestCase(8473, 4),
+    new TestCase(73746, 5),
+  ];
+  runTests("Two", testCases, numberOfDigits);
 }
 
 function runQ3Tests() {
@@ -138,70 +192,71 @@ function runQ3Tests() {
     new TestCase("aeaeae", ""),
     new TestCase("y doesn't count", "y dsn't cnt"),
     new TestCase("CAPITAL LETTERS DO COUNT", "CPTL LTTRS D CNT"),
-  ]
-  runTests("Three", testCases, disemvowel)
+  ];
+  runTests("Three", testCases, disemvowel);
 }
 
 function runQ4Tests() {
   let testCases = [
-    new TestCase([5,1,4,2,5,6], 2),
-    new TestCase([1,10,7,90,5,4], 4),
-    new TestCase([2,1,4,90,5,6], 2),
-    new TestCase([1,3,4,90,5,6], 3)
-  ]
-  runTests("Four", testCases, secondSmallest)
+    new TestCase([5, 1, 4, 2, 5, 6], 2),
+    new TestCase([1, 10, 7, 90, 5, 4], 4),
+    new TestCase([2, 1, 4, 90, 5, 6], 2),
+    new TestCase([1, 3, 4, 90, 5, 6], 3),
+  ];
+  runTests("Four", testCases, secondSmallest);
 }
 
 function runQ5Tests() {
   let testCases = [
     new TestCase(
       [
-        {location: "Algeria", population: 41},
-        {location: "Belize", population: 0.4},
-        {location: "China", population: 1386},
-        {location: "Denmark", population: 6}
+        { location: "Algeria", population: 41 },
+        { location: "Belize", population: 0.4 },
+        { location: "China", population: 1386 },
+        { location: "Denmark", population: 6 },
       ],
       ["Algeria", "Belize", "China", "Denmark"]
     ),
-    new TestCase([{location: "England", population: 56}], ["England"]),
-    new TestCase([], [])
-  ]
-  runTests("Five", testCases, getLocations)
+    new TestCase([{ location: "England", population: 56 }], ["England"]),
+    new TestCase([], []),
+  ];
+  runTests("Five", testCases, getLocations);
 }
 
 function runQ6Tests() {
   let testCases = [
-    new TestCase(
-      ["a", "bb", "ccc", "dddd", "eeeee"],
-      ["a", "ccc", "eeeee"]
-    ),
+    new TestCase(["a", "bb", "ccc", "dddd", "eeeee"], ["a", "ccc", "eeeee"]),
     new TestCase(
       ["Four", "score", "and", "seven", "years", "ago"],
       ["score", "and", "seven", "years", "ago"]
     ),
     new TestCase(
-      ["The", "only", "thing", "we", "have", "to", "fear", "is", "fear", "itself"],
-      ["The", "thing"],
+      [
+        "The",
+        "only",
+        "thing",
+        "we",
+        "have",
+        "to",
+        "fear",
+        "is",
+        "fear",
+        "itself",
+      ],
+      ["The", "thing"]
     ),
-    new TestCase(
-      ["one", "two", "three", "four"],
-      ["one", "two", "three"]
-    ),
-    new TestCase([],[]),
-    new TestCase(["a"],["a"]),
-    new TestCase(["to"],[])
-  ]
-  runTests("Six", testCases, onlyOddStrings)
+    new TestCase(["one", "two", "three", "four"], ["one", "two", "three"]),
+    new TestCase([], []),
+    new TestCase(["a"], ["a"]),
+    new TestCase(["to"], []),
+  ];
+  runTests("Six", testCases, onlyOddStrings);
 }
 
 function runQ7Tests() {
   let testCases = [
     new TestCase(
-      [
-        new Day(50, "raining"),
-        new Day(99, "sunny"),
-        new Day(24, "snowing")
-      ],
+      [new Day(50, "raining"), new Day(99, "sunny"), new Day(24, "snowing")],
       [
         "It is 50 degrees and raining",
         "It is 99 degrees and sunny",
@@ -209,16 +264,10 @@ function runQ7Tests() {
       ]
     ),
     new TestCase(
-      [
-        new Day(31, "sleeting"),
-        new Day(88, "partly cloudy")
-      ],
-      [
-        "It is 31 degrees and sleeting",
-        "It is 88 degrees and partly cloudy",
-      ]
+      [new Day(31, "sleeting"), new Day(88, "partly cloudy")],
+      ["It is 31 degrees and sleeting", "It is 88 degrees and partly cloudy"]
     ),
-    new TestCase([], [])
-  ]
-  runTests("Seven", testCases, getAllDayDescriptions)
+    new TestCase([], []),
+  ];
+  runTests("Seven", testCases, getAllDayDescriptions);
 }
